dd3898ed54f65bbb9f351dd2580be459cfee5eba
harluz <harluz06@gmail.com>
2021-06-07 12:35:45 +0900

    circleciの設定調整　rubocop調整

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        skip_before_action :verify_authenticity_token, if: :devise_controller?
-        # APIではCSRFチェックをしない
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
+  # APIではCSRFチェックをしない
 end
